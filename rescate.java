70a8f39c174765f815a688d6bfd8fc5b164365d2
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-19 13:31:49 +0100

    enunciado propuesto

---
+++ rescate.java
@@ -0,0 +1,16 @@
+/*
+ *En este ejercicio haremos un cuadrilatero y dentro de el otro con la mitad de sus dimensiones
+ *dentro del segundo podra estar o no la princesa y eso supone que hay que liberarla, tambien esta la posibilidad 
+ *de que alrrededor del cuarto haya vigilantes o no sengun la cantidad de ellos la dificultad para rescatar a la
+ *princesa sera mayor
+ * @author AntonioLuisGarcia
+ */
+import java.util.Scanner;
+public class rescate{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        sc.close();
+    }
+}
