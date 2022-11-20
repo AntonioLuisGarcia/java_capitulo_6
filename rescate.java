ccc1a1ae557d6fd143fb824be7a0e05155735c7c
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-20 23:23:21 +0100

    ejercicio nuevo planteado

--- rescate.java
+++ rescate.java
@@ -10,7 +10,46 @@ public class rescate{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        
+        System.out.println("¿De que dimensiones quieres que sea el castillo?");
+        System.out.print("Ancho: ");
+        int ancho=sc.nextInt();
+        System.out.print("Altura: ");
+        int altura=sc.nextInt();
+
+        //declaramos y le daos valores a las variables aleatorias
+
+        int princesa=(int)(Math.random()*10+1);
+        int guardias=(int)(Math.random()*ancho);
+        int comienzoXcuarto=(int)(Math.random()*((ancho-2)/2));
+        int comienzoYcuarto=(int)(Math.random()*((altura-2)/2));
+        int vecesarriba=0;
+        int vecesabajo=0;
+        boolean seguir=true;
+
+        for(int j=1;j<=ancho;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for(int i=1;i<=altura-2;i++){
+            seguir=false;
+            System.out.print("*");
+            for(int j=1;j<=ancho-2;j++){
+                if(i==comienzoYcuarto && j==comienzoXcuarto){
+                    seguir=true;
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("*");
+        }
+
+        for(int j=1;j<=ancho;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+
         sc.close();
     }
 }
