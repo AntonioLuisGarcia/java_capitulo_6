f74af4b0c964036e0b721a8fcb8f24d9c5608a7a
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-11-20 23:24:01 +0100

    ejercicio planteado

---
+++ horario.java
@@ -0,0 +1,16 @@
+/*
+ * El programa consiste en generar un horario de las asignaturas que tenemos, 
+ *de manera que los viernes solo haya 3 horas de FOL y 3 horas de SINF. No 
+ *hay que preocuparse porque las horas estén seguidas, solo en el caso de los 
+ *viernes. Se pedirá al usuario de qué asignatura es el profesor que solo da 
+ *clases en horario previo al recreo.
+ * 
+ * @author Antonio Luis Garcia
+ */
+
+
+public class horario{
+	public static void main(String[] args){
+
+        }
+	}
